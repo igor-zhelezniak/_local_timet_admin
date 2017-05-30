b13f612bd0f1242bcbd7808e30257086015e6e9e
Igor Zhelezniak <mosakovskijj@yandex.ru>
2017-05-30 12:13:13 +0300

    cropper fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -567,7 +567,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         $('input[name=userProfileFile]').on('change', function () {
 
             readURL(this,function () {
-                $('.profile-user-img').cropper({
+                $('.profile-user-img').cropper('destroy').cropper({
                     aspectRatio: 1,
                     crop: function(e) {
                         object.x = e.x;
